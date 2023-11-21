e2568c6af4b361fce14b096f1540ae92a098976f
Difficins <a.kolomijec@seznam.cz>
2023-11-21 21:51:31 +0100

    Basic logic finished

--- src/main.js
+++ src/main.js
@@ -6,51 +6,44 @@
 // const { Gameboard } = require("./gameboard");
 
 import "./style.css";
+import Battleship from "./battleship.jpg";
 import Ship from "./ship";
 import Gameboard from "./gameboard";
 
 const enemyBoard = document.querySelector("#gameboard-enemy");
 const playerBoard = document.querySelector("#gameboard-player");
 const buttonPlaceFleet = document.querySelector("#place-fleet");
+const buttonStart = document.querySelector("#start");
+const textField = document.querySelector("#text");
 
-const boardPlayer = new Gameboard(10, 10);
-const boardEnemy = new Gameboard(10, 10);
+let boardPlayer = new Gameboard(10, 10);
+let boardEnemy = new Gameboard(10, 10);
 
-const submarine1 = new Ship(1);
-const submarine2 = new Ship(1);
-const destroyer1 = new Ship(2);
-const destroyer2 = new Ship(2);
-const cruiser = new Ship(3);
-const battleship = new Ship(4);
-const carrier = new Ship(5);
+let ships = {};
+let shipsEnemy = {};
 
-const submarine1Enemy = new Ship(1);
-const submarine2Enemy = new Ship(1);
-const destroyer1Enemy = new Ship(2);
-const destroyer2Enemy = new Ship(2);
-const cruiserEnemy = new Ship(3);
-const battleshipEnemy = new Ship(4);
-const carrierEnemy = new Ship(5);
+const initializeShips = () => {
+  ships = {
+    submarine1: new Ship(1),
+    submarine2: new Ship(1),
+    destroyer1: new Ship(2),
+    destroyer2: new Ship(2),
+    cruiser: new Ship(3),
+    battleship: new Ship(4),
+    carrier: new Ship(5),
+  };
+  shipsEnemy = {
+    submarine1Enemy: new Ship(1),
+    submarine2Enemy: new Ship(1),
+    destroyer1Enemy: new Ship(2),
+    destroyer2Enemy: new Ship(2),
+    cruiserEnemy: new Ship(3),
+    battleshipEnemy: new Ship(4),
+    carrierEnemy: new Ship(5),
+  };
+};
 
-const ships = [
-  submarine1,
-  submarine2,
-  destroyer1,
-  destroyer2,
-  cruiser,
-  battleship,
-  carrier,
-];
-
-const shipsEnemy = [
-  submarine1Enemy,
-  submarine2Enemy,
-  destroyer1Enemy,
-  destroyer2Enemy,
-  cruiserEnemy,
-  battleshipEnemy,
-  carrierEnemy,
-];
+initializeShips();
 
 const createPlayerBoard = (() => {
   for (let row = 0; row < 10; row++) {
@@ -91,31 +84,54 @@ function displayShips(board, enemy = false) {
   }
 }
 
-function randomPlacement(gameboard, fleet) {
-  for (const ship of fleet) {
+function randomPlacement(board, fleet) {
+  for (const ship in fleet) {
     let row = Math.floor(Math.random() * 10);
     let col = Math.floor(Math.random() * 10);
     let orientation = ["vertical", "horizontal"][Math.random() < 0.5 ? 0 : 1];
-    while (!gameboard.isValidPlacement(ship, row, col, orientation)) {
+    while (!board.isValidPlacement(fleet[ship], row, col, orientation)) {
       row = Math.floor(Math.random() * 10);
       col = Math.floor(Math.random() * 10);
       orientation = ["vertical", "horizontal"][Math.random() < 0.5 ? 0 : 1];
     }
-    gameboard.placeShip(ship, row, col, orientation);
+    board.placeShip(fleet[ship], row, col, orientation);
   }
 }
 
-function wipeBoard(gameboard) {
-  for (let row = 0; row < 10; row++) {
-    for (let col = 0; col < 10; col++) {
-      const cell = document.getElementById(`${row}-${col}`);
-      cell.classList.remove("ship");
-      gameboard.grid[row][col] = null;
+function wipeBoards() {
+  const hitCells = document.querySelectorAll(".hit, .ship, .ship-enemy");
+  hitCells.forEach((cell) => {
+    cell.classList.remove("ship");
+    cell.classList.remove("hit");
+    cell.classList.remove("ship-enemy");
+  });
+  boardPlayer = new Gameboard(10, 10);
+  boardEnemy = new Gameboard(10, 10);
+  cellsEnemy.forEach((i) => i.removeEventListener("click", handlePlayerClick));
+  initializeShips();
+}
+
+function showSunkFleet(fleet, enemy = false) {
+  for (const ship in fleet) {
+    if (fleet[ship].isSunk() && !enemy) {
+      const shownSunkShip = document.querySelector(`.${ship}-player`);
+      const childDivs = shownSunkShip.querySelectorAll("div");
+      childDivs.forEach((child) => {
+        child.classList.add("hit");
+        child.classList.add("ship");
+      });
+    } else if (fleet[ship].isSunk() && enemy) {
+      const shownSunkShip = document.querySelector(
+        `.${ship.slice(0, -5)}-enemy`,
+      );
+      const childDivs = shownSunkShip.querySelectorAll("div");
+      childDivs.forEach((child) => {
+        child.classList.add("hit");
+        child.classList.add("ship");
+      });
     }
   }
 }
-
-function showFleet(gameboard) {}
 
 randomPlacement(boardPlayer, ships);
 displayShips(boardPlayer);
@@ -124,25 +140,57 @@ randomPlacement(boardEnemy, shipsEnemy);
 displayShips(boardEnemy, true);
 
 buttonPlaceFleet.addEventListener("click", () => {
-  wipeBoard(boardPlayer);
+  textField.innerText = "Place your fleet!";
+  wipeBoards();
   randomPlacement(boardPlayer, ships);
+  randomPlacement(boardEnemy, shipsEnemy);
   displayShips(boardPlayer);
+  displayShips(boardEnemy, true);
+  window.ships = shipsEnemy;
+  window.board = boardPlayer;
+  window.board2 = boardEnemy;
 });
 
-cells.forEach((cell) =>
-  cell.addEventListener("click", (e) => {
-    e.target.classList.add("hit");
-    boardPlayer.receiveAttack(e.target.id[0], e.target.id[2]);
-  }),
-);
+function playerAttack(e) {
+  e.target.classList.add("hit");
+  boardEnemy.receiveAttack(e.target.id[0], e.target.id[2]);
+  showSunkFleet(shipsEnemy, true);
+}
 
-cellsEnemy.forEach((cell) =>
-  cell.addEventListener("click", (e) => {
-    e.target.classList.add("hit");
-    boardEnemy.receiveAttack(e.target.id[0], e.target.id[2]);
-  }),
-);
+function computerAttack() {
+  let row = Math.floor(Math.random() * 10);
+  let col = Math.floor(Math.random() * 10);
+  while (boardPlayer.attackedCells.includes(`${row}-${col}`)) {
+    row = Math.floor(Math.random() * 10);
+    col = Math.floor(Math.random() * 10);
+  }
+  const target = document.getElementById(`${row}-${col}`);
+  target.classList.add("hit");
+  boardPlayer.receiveAttack(row, col);
+  showSunkFleet(ships);
+}
 
-window.ships = ships;
+function handlePlayerClick(e) {
+  if (
+    !boardEnemy.attackedCells.includes(`${e.target.id[0]}-${e.target.id[2]}`)
+  ) {
+    playerAttack(e);
+    computerAttack();
+  }
+}
+
+function gameplay() {
+  textField.innerText = "Game has started!";
+  function setupPlayerTurn() {
+    cellsEnemy.forEach((cell) =>
+      cell.addEventListener("click", handlePlayerClick),
+    );
+  }
+  setupPlayerTurn();
+}
+
+buttonStart.addEventListener("click", gameplay);
+
+window.ships = shipsEnemy;
 window.board = boardPlayer;
 window.board2 = boardEnemy;
