12a7ba3ae97ab726e9ef4d247e28d099806a2434
Difficins <a.kolomijec@seznam.cz>
2023-11-26 20:32:53 +0100

    Deleted

--- src/tests/ship.test.js
+++
@@ -1,9 +0,0 @@
-// const Ship = require("../ship");
-
-// test("Tests hits to the ship.", () => {
-//   const ship = new Ship(7);
-//   ship.hit();
-//   ship.hit();
-//   ship.hit();
-//   expect(ship.hits).toBe(3);
-// });
